c9928fa46ed505c7ccef821ae4906a1ce25f04bb
Adam Gabryś <adam.gabrys@sap.com>
2017-07-03 12:55:00 +0200

    Add S2175 false positive

---
+++ src/main/java/biz/gabrys/agabrys/sonarqube/falsepositives/d20170703/S2175.java
@@ -0,0 +1,60 @@
+package biz.gabrys.agabrys.sonarqube.falsepositives.d20170703;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public class S2175 {
+
+    @SuppressWarnings("unchecked")
+    public void collectModifiedEntriesInternal(final Object value, final Collection<AbstractItemModel> result) {
+        final boolean isItem = value instanceof AbstractItemModel;
+        final boolean isCollection = !isItem && value instanceof Collection;
+        final boolean isMap = !isCollection && value instanceof Map;
+        if (isItem && canPersistSingleElement(value)) {
+            if (!result.contains(value)) {
+                result.add((AbstractItemModel) value);
+                collectModifiedEntities((AbstractItemModel) value, result);
+            }
+        } else if (isCollection) {
+            ((Collection) value).stream().filter(element -> element instanceof AbstractItemModel && !result.contains(element))
+                    .forEach(element -> {
+                        result.add((AbstractItemModel) element);
+                        collectModifiedEntities((AbstractItemModel) element, result);
+                    });
+        } else if (isMap) {
+            final Set<Map.Entry> set = ((Map) value).entrySet();
+            for (final Map.Entry entry : set) {
+                final Object keyValue = entry.getKey();
+                if (canPersistSingleElement(keyValue)) {
+                    if (!result.contains(keyValue)) {
+                        result.add((AbstractItemModel) keyValue);
+                        collectModifiedEntities((AbstractItemModel) keyValue, result);
+                    }
+                } else {
+                    collectModifiedEntriesInternal(keyValue, result);
+                }
+                final Object mapValue = entry.getValue();
+                if (mapValue instanceof AbstractItemModel) {
+                    collectModifiedEntities((AbstractItemModel) mapValue, result);
+                } else {
+                    collectModifiedEntriesInternal(mapValue, result);
+                }
+            }
+        }
+    }
+
+    // MOCKS
+    private boolean canPersistSingleElement(final Object value) {
+        return new Random().nextBoolean();
+    }
+
+    private void collectModifiedEntities(final AbstractItemModel value, final Collection<AbstractItemModel> result) {
+        // do nothing
+    }
+
+    public interface AbstractItemModel {
+
+    }
+}
