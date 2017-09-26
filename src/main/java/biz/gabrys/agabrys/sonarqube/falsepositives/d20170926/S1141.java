55dd11d9ea59ac7e4f5cbcb506652e7299312664
Adam Gabryś <adam.gabrys@sap.com>
2017-09-26 11:09:45 +0200

    Add S1141 false positive

---
+++ src/main/java/biz/gabrys/agabrys/sonarqube/falsepositives/d20170926/S1141.java
@@ -0,0 +1,49 @@
+package biz.gabrys.agabrys.sonarqube.falsepositives.d20170926;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+
+public class S1141 {
+
+    private static final Logger LOG = Logger.getLogger(S1141.class.getName());
+    private final ConcurrentMap<Set<Class<?>>, JAXBContext> contextCache = new ConcurrentHashMap<Set<Class<?>>, JAXBContext>();
+
+    public JAXBContext createContext(final Class<?>... jaxbClasses) throws JAXBException {
+        if (jaxbClasses == null || jaxbClasses.length == 0) {
+            throw new IllegalArgumentException("JAXB class list for context must not be null");
+        }
+        final Set<Class<?>> key = new HashSet<>(Arrays.asList(jaxbClasses));
+        try {
+            return contextCache.computeIfAbsent(key, k -> {
+                try {
+                    return JAXBContext.newInstance(jaxbClasses);
+                } catch (final JAXBException e) {
+                    throw new JAXBRuntimeException(e);
+                }
+            });
+        } catch (final JAXBRuntimeException e) {
+            LOG.log(Level.WARNING, "Cannot create JAXB Context", e);
+            throw e.getTypedCause();
+        }
+    }
+
+    @SuppressWarnings("serial")
+    private static class JAXBRuntimeException extends RuntimeException {
+
+        public JAXBRuntimeException(final JAXBException cause) {
+            super(cause);
+        }
+
+        public JAXBException getTypedCause() {
+            return (JAXBException) getCause();
+        }
+    }
+}
