6f0050b81fe207f6113b74000dbe7f070b9c815a
agabrys <adam.gabrys@live.com>
2018-01-06 21:53:47 +0100

    add a link to the issue in the Checkstyle tracker system

--- src/main/java/biz/gabrys/agabrys/sonarqube/falsepositives/d20180106/HideUtilityClassConstructorCheck.java
+++ src/main/java/biz/gabrys/agabrys/sonarqube/falsepositives/d20180106/HideUtilityClassConstructorCheck.java
@@ -3,6 +3,9 @@ package biz.gabrys.agabrys.sonarqube.falsepositives.d20180106;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @see https://github.com/checkstyle/checkstyle/issues/5434
+ */
 @SpringBootApplication
 public class HideUtilityClassConstructorCheck {
 
