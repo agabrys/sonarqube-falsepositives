65bd0062a9098bca1e4e17a73065127e44e74509
Adam Gabryś <adam.gabrys@sap.com>
2018-03-05 16:06:03 +0100

    S2386 does not recognize Singleton set, list and map

---
+++ src/main/java/biz/gabrys/agabrys/sonarqube/falsepositives/d20180305/S2386.java
@@ -0,0 +1,15 @@
+package biz.gabrys.agabrys.sonarqube.falsepositives.d20180305;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface S2386 {
+
+    Set<Object> SINGLETON_UNMODIFICABLE_SET = Collections.singleton(new Object());
+
+    List<Object> SINGLETON_UNMODIFICABLE_LIST = Collections.singletonList(new Object());
+
+    Map<Object, Object> SINGLETON_UNMODIFICABLE_MAP = Collections.singletonMap(new Object(), new Object());
+}
