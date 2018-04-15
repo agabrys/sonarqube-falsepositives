8f114605ba2d3d10de500ba1010877e69384d305
agabrys <adam.gabrys@live.com>
2018-04-15 20:09:58 +0200

    add S2168 Double-checked locking should not be used - False Positive - it doesn't work when an assignment operation is in another method

---
+++ src/main/java/biz/gabrys/agabrys/sonarqube/falsepositives/d20180415/S2168.java
@@ -0,0 +1,88 @@
+package biz.gabrys.agabrys.sonarqube.falsepositives.d20180415;
+
+/**
+ * @see https://stackoverflow.com/q/49741933/4944847
+ */
+public interface S2168 {
+    // do nothing
+}
+
+class VolatileAssigmentInTheSameMethod {
+
+    private static volatile Object instance;
+    private static final Object lock = new Object();
+
+    public static Object getInstance() {
+        if (instance == null) {
+            // No issue
+            synchronized (lock) {
+                if (instance == null) {
+                    instance = new Object();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+class NonVolatileAssigmentInTheSameMethod {
+
+    private static Object instance;
+    private static final Object lock = new Object();
+
+    public static Object getInstance() {
+        if (instance == null) {
+            // Issue (OK)
+            synchronized (lock) {
+                if (instance == null) {
+                    instance = new Object();
+                }
+            }
+        }
+        return instance;
+    }
+}
+
+class VolatileAssigmentInOtherMethod {
+
+    private static volatile Object instance;
+    private static final Object lock = new Object();
+
+    public static Object getInstance() {
+        // No issue (OK)
+        if (instance == null) {
+            synchronized (lock) {
+                if (instance == null) {
+                    create();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private static void create() {
+        instance = new Object();
+    }
+}
+
+class NonVolatileAssigmentInOtherMethod {
+
+    private static Object instance;
+    private static final Object lock = new Object();
+
+    public static Object getInstance() {
+        if (instance == null) {
+            // No issue (False Positive)
+            synchronized (lock) {
+                if (instance == null) {
+                    create();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private static void create() {
+        instance = new Object();
+    }
+}
